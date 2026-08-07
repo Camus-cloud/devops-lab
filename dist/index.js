2bbe14ff4ac810a90ea3d8863927b945cb2e4648
Camus-cloud <nouhan1990@gmail.com>
2026-08-07 20:50:30 +0000

    Turn index.js into a persistent HTTP server instead of one-shot script

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from prod (non-root)!")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from prod (non-root)!\n');
+});
+
+const PORT = process.env.PORT || 80;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
